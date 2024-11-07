98c255917a87d062f7b5b837e2f0f31c98de77d8
PKamilly <pkamillymattana@gmail.com>
2024-11-07 11:27:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const G = 6.67430e-11; // Constante gravitacional (m^3 kg^-1 s^-2)
 const mPlaneta = 5.972e24; // Massa do planeta (kg)
 const mLua = 7.34767309e22; // Massa da Lua (kg)
 const distancia = 384400000; // Distância média entre o planeta e a Lua (m)
+const r = 30;
 
 // Função para calcular a velocidade orbital
 function calcularVelocidadeOrbital(r) {
