2a4a69642e17bbdfd0a423da5922590630fc2fc6
PKamilly <pkamillymattana@gmail.com>
2024-11-07 11:20:56 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Constantes de física
-const G = 6.67430e-11; // Constante gravitacional (m^3 kg^-1 s^-2)
+const G = 6; // Constante gravitacional (m^3 kg^-1 s^-2)
 const mPlaneta = 5.972e24; // Massa do planeta (kg)
 const mLua = 7.34767309e22; // Massa da Lua (kg)
 const distancia = 384400000; // Distância média entre o planeta e a Lua (m)
